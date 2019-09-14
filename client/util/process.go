e9b5c115fab68695fad62a1a3893072d0b4df6ea
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-09-14 19:24:50 +0700

    Add: GetInfo and fix GetProcessByName in client/util/process

--- client/util/process.go
+++ client/util/process.go
@@ -1,14 +1,14 @@
-package process
+package util
 
 import (
+	"healthcheck/client/model"
+
+	"github.com/shirou/gopsutil/host"
+	"github.com/shirou/gopsutil/mem"
 	"github.com/shirou/gopsutil/process"
 )
 
-func GetProcessByName(name string) ([]*process.Process, error) {
-	procs, err := process.Processes()
-	if err != nil {
-		return nil, err
-	}
+func GetProcessByName(procs []*process.Process, name string) ([]*process.Process, error) {
 	var filterProcs []*process.Process
 	for _, proc := range procs {
 		if procName, err := proc.Name(); err != nil {
@@ -18,4 +18,29 @@ func GetProcessByName(name string) ([]*process.Process, error) {
 		}
 	}
 	return filterProcs, error(nil)
+}
+
+func GetInfo(procs []*process.Process) model.HostInfo {
+	bootTime, _ := host.BootTime()
+	upTime, _ := host.Uptime()
+	memInfoStat, _ := mem.VirtualMemory()
+	var processesInfo []model.ProcessInfo
+	for _, proc := range procs {
+		var processInfo model.ProcessInfo
+		processInfo.Name, _ = proc.Name()
+		processInfo.CPUPercent, _ = proc.CPUPercent()
+		processInfo.CPUAffinity, _ = proc.CPUAffinity()
+		processInfo.Cmdline, _ = proc.Cmdline()
+		processInfo.Exe, _ = proc.Exe()
+		processInfo.MemoryInfoStat, _ = proc.MemoryInfo()
+		processInfo.ConnStats, _ = proc.Connections()
+
+		processesInfo = append(processesInfo, processInfo)
+	}
+	return model.HostInfo{
+		BootTime:          bootTime,
+		Uptime:            upTime,
+		VirtualMemoryStat: *memInfoStat,
+		ProcessesInfo:     processesInfo,
+	}
 }
--- client/util/process_test.go
+++ client/util/process_test.go
@@ -1,24 +1,34 @@
-package process_test
+package util_test
 
 import (
-	. "healthcheck/util"
+	"healthcheck/client/model"
+	. "healthcheck/client/util"
 	"testing"
 
 	"github.com/shirou/gopsutil/process"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetProcessByName(t *testing.T) {
-	allProcs, err := process.Processes()
-	assert.Nil(t, err)
-	assert.NotEmpty(t, allProcs)
+func TestGetProcessesInfo(t *testing.T) {
+	procs, _ := process.Processes()
+	filterProcsByName, _ := GetProcessByName(procs, "go")
+	hostInfo := GetInfo(filterProcsByName)
+	assert.NotNil(t, hostInfo)
+	assert.NotEmpty(t, hostInfo)
+	assert.IsType(t, model.HostInfo{}, hostInfo)
+}
 
-	filterProcsByName, err := GetProcessByName("go.exe")
+func TestGetProcessByName(t *testing.T) {
+	procs, err := process.Processes()
+	assert.Nil(t, err)
+	assert.NotEmpty(t, procs)
+
+	filterProcsByName, err := GetProcessByName(procs, "go")
 	assert.Nil(t, err)
 	assert.NotEmpty(t, filterProcsByName)
 
-	assert.Greater(t, len(allProcs), len(filterProcsByName))
+	assert.Greater(t, len(procs), len(filterProcsByName))
 	for _, proc := range filterProcsByName {
-		assert.Contains(t, allProcs, proc)
+		assert.Contains(t, procs, proc)
 	}
 }
